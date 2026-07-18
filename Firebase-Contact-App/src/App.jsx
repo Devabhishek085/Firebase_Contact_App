761df20787039b22c6aecdbf3e632315eaf9601b
Abhishek Pal <palabhishek085@gmail.com>
2026-07-19 00:19:56 +0530

    Interface

--- Firebase-Contact-App/src/App.jsx
+++ Firebase-Contact-App/src/App.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
 import { FaSearch } from "react-icons/fa";
+import { FaCirclePlus } from "react-icons/fa6";
 
 const App = () => {
+
+  const [contacts, setcontacts] = useState([])
+
+  useEffect(()=>{
+    
+  },[])
+
+
+
+
+
   return (
     <div className='mx-w-[370px] mx-auto'>
       <Navbar/>
-      <div className='flex relative items-center'>
+      <div className='flex gap-2'>
+              <div className='flex relative items-center flex-grow '>
+
         <FaSearch className='text-white text-3xl absolute ml-1'/>
+
         <input type="text"  className='h-10 flex-grow rounded-md border border-white bg-transparent text-white pl-9'/>
-        
+      </div>
+
+     
+        <FaCirclePlus className='text-5xl text-white cursor-pointer'/>
+    
       </div>
     </div>
   )
--- Firebase-Contact-App/src/config/firebase.js
+++ Firebase-Contact-App/src/config/firebase.js
@@ -1,19 +1,15 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB1qsdkQk0XQpLvV2OQ-UWP151lZTuBclE",
-  authDomain: "fir-contact-app-3c52c.firebaseapp.com",
-  projectId: "fir-contact-app-3c52c",
-  storageBucket: "fir-contact-app-3c52c.firebasestorage.app",
-  messagingSenderId: "118410532530",
-  appId: "1:118410532530:web:994710b9c91b88c9bd9bab"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db=getFirestore(app);
